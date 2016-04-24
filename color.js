dfbd98925aad807171a6af930fe73e539fa6cb04
bogicevic <zbogicevic@outlook.com>
2016-04-25 01:02:34 +0200

    Added message span and randomized color items

--- color.js
+++ color.js
@@ -6,8 +6,9 @@ var colors = ["rgb(255, 0, 0)",
               "rgb(255, 0, 255)"];
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,9 +24,24 @@ for(var i = 0; i < squares.length; i++){
     var clickedColor = clickedColorFullString.slice(0, clickedColorFullString.indexOf(")") + 1);
     // compare color to pickedColor
     if(clickedColor === pickedColor) {
-      alert("correct");
+      messageDisplay.textContent = "Correct!";
+      changeColors(clickedColor);
     } else {
-      alert("wrong");
+      this.style.background = "#232323";
+      messageDisplay.textContent = "Try Again";
     }
   });
+}
+
+function changeColors(color){
+  // loop through all squares
+  // change each color to match given color
+  for(var i = 0; i < squares.length; i++) {
+    squares[i].style.background = color;
+  }
+}
+
+function pickColor(){
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
 }
